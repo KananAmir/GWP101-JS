16f0d63c440499910fc1080f0970310a6d10dddd
KananAmir <amirovknn@gmail.com>
2023-04-05 12:18:04 +0400

    js intro

---
+++ 05.04.JS-Variables.DataTypes.Operators/main.js
@@ -0,0 +1,52 @@
+//DATA TYPES
+let fullName = "Amirov Kanan";
+
+let age = 56;
+
+let examScore = 55;
+
+let isOnline = false;
+
+let isMarried = true;
+
+let university;
+
+// console.log(fullName);
+// console.log(isOnline);
+// console.log(university);
+
+let test = null;
+// console.log(test);
+
+// VARIABLES
+
+// let, const, var
+
+// var firsName = "Kanan";
+// var lastName = "Amirov";
+
+let firsName = "Kanan";
+let lastName = "Amirov";
+
+firsName = "Test";
+// console.log(firsName);
+
+// {
+//   let url = "www.test.com";
+// }
+
+console.log(url);
+var url = "www.test.com";
+
+let finalExamScore = 90;
+
+finalExamScore = 88;
+console.log(finalExamScore);
+
+const MAIN_PASSWORD = "12345678";
+
+MAIN_PASSWORD = "VHBJASBjh";
+
+console.log(MAIN_PASSWORD);
+
+// let FullName;
---
+++ 05.04.JS-Variables.DataTypes.Operators/operators.js
@@ -0,0 +1,63 @@
+// let fullName = "Amirov Kanan";
+
+// console.log(fullName);
+
+// // window.alert("Hellooo");
+
+// document.write("Hello World");
+
+//ARICHMETIC OPERATORS
+
+let firstNumber = 15;
+let secondNumber = 7;
+
+let sum, subtruct, multiply, division, power, qalıq;
+
+sum = firstNumber + secondNumber;
+subtruct = firstNumber - secondNumber;
+multiply = firstNumber * secondNumber;
+division = firstNumber / secondNumber;
+qalıq = firstNumber % secondNumber;
+power = firstNumber ** secondNumber;
+
+console.log(
+  "Sum: ",
+  sum,
+  "Sub: ",
+  subtruct,
+  "Multiply: ",
+  multiply,
+  "Division: ",
+  division,
+  "Qaliq: ",
+  qalıq,
+  "Power: ",
+  power
+);
+
+let count = 5;
+
+// count++;
+count--;
+
+// count = count + 1;
+// count = count - 1;
+
+console.log(count);
+
+let score = 55;
+
+// score /= 6;
+
+score = score / 6;
+console.log(score);
+
+//BITWISE OPERATORS
+
+// console.log(16 & 7);
+// console.log(2 | 7);
+
+//Shift Assignment Operators
+// console.log(5 >> 1);
+
+console.log("7 >> 2: ", 7 >> 2);
